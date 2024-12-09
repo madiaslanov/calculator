6b701b98071de0a531e2c9623dc0131cff76d4ee
Madi <asmadi.rus@mail.ru>
2024-12-09 23:00:18 +0500

    first commit

---
+++ javascriptIos.js
@@ -0,0 +1,104 @@
+let currentInput = '0';
+let previousInput = '';
+let operator = '';
+const MAX_INPUT_LENGTH = 17;
+
+function updateDisplay() {
+  document.querySelector('.ios-display').textContent = currentInput;
+}
+
+function appendClear() {
+  currentInput = '0';
+  previousInput = '';
+  operator = '';
+  updateDisplay();
+}
+
+function appendNumber(number) {
+  if (currentInput.length < MAX_INPUT_LENGTH) {
+    if (currentInput === '0') {
+      currentInput = number.toString();
+    } else {
+      currentInput += number.toString();
+    }
+    updateDisplay();
+  }
+}
+
+function appendOperator(op) {
+  if (previousInput === '') {
+    previousInput = currentInput;
+    operator = op;
+    currentInput = '0'; 
+  } else {
+    calculateResult();
+    operator = op;
+  }
+}
+
+function appendDot() {
+
+  if (!currentInput.includes('.') && currentInput.length < MAX_INPUT_LENGTH) {
+    currentInput += '.';
+  }
+  updateDisplay();
+}
+
+function calculateResult() {
+  if (previousInput === '') return;
+
+  let result;
+  const prev = parseFloat(previousInput);
+  const current = parseFloat(currentInput);
+
+  switch (operator) {
+    case '+':
+      result = prev + current;
+      break;
+
+    case '-':
+      result = prev - current;
+      break;
+
+    case '/':
+      if (current === 0) {
+        alert('Error! Division by zero.');
+        result = '0';
+      } else {
+        result = prev / current;
+      }
+      break;
+
+    case '*':
+      result = prev * current;
+      break;
+
+    case '%':
+      result = (prev * current) / 100;
+      break;
+
+    default:
+      return;
+  }
+
+  currentInput = result.toString().slice(0, MAX_INPUT_LENGTH);
+  previousInput = '';
+  operator = '';
+  updateDisplay();
+}
+
+function toggleSign() {
+  if (currentInput.startsWith('-')) {
+    currentInput = currentInput.substring(1);
+  } else {
+    currentInput = '-' + currentInput;
+  }
+  updateDisplay();
+}
+
+function appendPercentage() {
+  if (currentInput !== '0') {
+    currentInput = (parseFloat(currentInput) / 100).toString();
+    updateDisplay();
+  }
+}
